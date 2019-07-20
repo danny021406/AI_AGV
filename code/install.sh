7b4ff916117cff569b2d7938ea3863fe5741cf63
6f6d55aa-7242-4f2b-9e94-cc5a0de9a4b0 <danny021406@gmail.com>
2019-07-20 12:58:34 +0800

    add install.sh

---
+++ code/install.sh
@@ -0,0 +1,9 @@
+sudo pip install keras==1.2
+sudo pip install h5py
+sudo pip install opencv-python
+sudo apt-get update
+sudo apt-get install libsm6
+sudo apt-get install libxrender1
+sudo apt-get install libxext-dev
+sudo pip install moviepy
+sudo apt install python3-tk
